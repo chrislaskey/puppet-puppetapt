a7af3fe1f3a3afaebeaa8323e0d771ce415088a0
Chris Laskey <contact@chrislaskey.com>
2013-11-03 15:05:08 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+About
+================================================================================
+
+The `puppetapt` module adds the Puppet Labs puppet apt repositories
+([http://apt.puppetlabs.com/](http://apt.puppetlabs.com/)) to the apt sources
+list.
+
+This adds versions of puppet packages like `puppet`, `puppetmaster`, and
+`puppetdb` amongst others.
+
+License
+================================================================================
+
+All code written by me is released under MIT license. See the attached
+license.txt file for more information, including commentary on license choice.
---
+++ templates/install-puppetapt.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+this_file=`basename "$0"`
+deb_file_url="<%= deb_file_url %>"
+# deb_file_url="http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb"
+deb_file_name="<%= deb_file_name %>"
+# deb_file_name="puppetlabs-release-wheezy.deb"
+deb_file_dir="/tmp/"
+deb_file_path="${deb_file_dir}${deb_file_name}"
+apt_source_file="/etc/apt/sources.list.d/puppetlabs.list"
+
+set -o nounset
+set -o errtrace
+set -o errexit
+set -o pipefail
+
+# General functions
+
+log () {
+	printf "$*\n"
+}
+
+error () {
+	log "ERROR: " "$*\n"
+	exit 1
+}
+
+help () {
+	echo "Usage is './${this_file} <project-name>'"
+}
+
+verify_root_privileges () {
+	if [[ $EUID -ne 0 ]]; then
+		fail "Requires root privileges."
+	fi
+}
+
+before_exit () {
+	# Works like a finally statement
+	# Code that must always be run goes here
+	if [[ -f "$deb_file_path" ]]; then
+		/bin/rm "$deb_file_path"
+	fi
+} ; trap before_exit EXIT
+
+# Application functions
+
+exit_if_already_completed () {
+	if dpkg -s puppetlabs-release > /dev/null 2>&1; then
+		log "NOTICE: Puppetlabs release package already installed. Exiting successfully."
+		exit 0
+	fi
+}
+
+get_deb_file () {
+	if ! wget -P "$deb_file_dir" "$deb_file_url"; then
+		error "Failed to download file ${deb_file_url}"
+	fi
+}
+
+install_deb_file () {
+	if ! dpkg -i "$deb_file_path"; then
+		error "Failed to install file ${deb_file_path}"
+	fi
+}
+
+update_apt () {
+	if ! apt-get update; then
+		error "Apt failed to update"
+	fi
+}
+
+# Application execution
+
+verify_root_privileges
+exit_if_already_completed
+get_deb_file
+install_deb_file
+update_apt
