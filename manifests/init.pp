f4e41b9537a3ea8a5214e7f87ae75aefe600f20b
Chris Laskey <contact@chrislaskey.com>
2013-11-03 15:14:22 -0500

    Added $::osfamily check for Debian

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,38 +1,43 @@
 class puppetapt (
 	$release,
+	# For example "wheezy" "squeeze" (Debian) or "precise" (Ubuntu)
 	# See http://apt.puppetlabs.com/ for a list of available releases
 ){
 
-	$deb_file_name = "puppetlabs-release-${release}.deb"
-	$deb_file_url = "http://apt.puppetlabs.com/${deb_file_name}"
+	if $::osfamily == "Debian" {
 
-	file { "/etc/puppet/scripts":
-		ensure => "directory",
-		owner => "puppet",
-		group => "puppet",
-		mode => "0700",
-	}
+		$deb_file_name = "puppetlabs-release-${release}.deb"
+		$deb_file_url = "http://apt.puppetlabs.com/${deb_file_name}"
 
-	file { "/etc/puppet/scripts/install-puppetapt.sh":
-		ensure => "present",
-		content => template("puppetapt/install-puppetapt.sh"),
-		owner => "puppet",
-		group => "puppet",
-		mode => "0700",
-		require => [
-			File["/etc/puppet/scripts"],
-		]
-	}
+		file { "/etc/puppet/scripts":
+			ensure => "directory",
+			owner => "puppet",
+			group => "puppet",
+			mode => "0700",
+		}
 
-	exec { "install-puppetapt.sh":
-		command => "/etc/puppet/scripts/install-puppetapt.sh",
-		path => "/bin:/sbin:/usr/bin:/usr/sbin",
-		user => "root",
-		group => "root",
-		logoutput => "on_failure",
-		require => [
-			File["/etc/puppet/scripts/install-puppetapt.sh"],
-		],
+		file { "/etc/puppet/scripts/install-puppetapt.sh":
+			ensure => "present",
+			content => template("puppetapt/install-puppetapt.sh"),
+			owner => "puppet",
+			group => "puppet",
+			mode => "0700",
+			require => [
+				File["/etc/puppet/scripts"],
+			]
+		}
+
+		exec { "install-puppetapt.sh":
+			command => "/etc/puppet/scripts/install-puppetapt.sh",
+			path => "/bin:/sbin:/usr/bin:/usr/sbin",
+			user => "root",
+			group => "root",
+			logoutput => "on_failure",
+			require => [
+				File["/etc/puppet/scripts/install-puppetapt.sh"],
+			],
+		}
+
 	}
 
 }
